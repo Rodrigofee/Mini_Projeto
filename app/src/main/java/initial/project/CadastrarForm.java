b97bd05d59981114682febec5993dcb8b0aecb26
Rodrigofee <rcabral756@gmail.com>
2019-09-20 20:20:46 -0300

    Finalizando

--- app/src/main/java/initial/project/CadastrarForm.java
+++ app/src/main/java/initial/project/CadastrarForm.java
@@ -1,14 +1,37 @@
+
 package initial.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CadastrarForm extends AppCompatActivity {
+
+    String content;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastrar_form);
+
+
+        Button cButton = findViewById(R.id.BottonCadastrar);
+        cButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                content = ((EditText) findViewById(R.id.text_nome)).getText().toString();
+
+                Intent intent = new Intent(getApplicationContext(), ListaDados.class);
+                intent.putExtra("message_key", content);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
--- app/src/main/java/initial/project/ListaDados.java
+++ app/src/main/java/initial/project/ListaDados.java
@@ -5,20 +5,43 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListaDados extends AppCompatActivity {
 
+    ListView lista;
+    List<String> listaDados;
+    ArrayAdapter<String> adaptador;
+    String newTexto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_dados);
+
+
+    lista = (ListView) findViewById(R.id.lista);
+    listaDados = new ArrayList<String>();
+    adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listaDados);
+
+    newTexto = getIntent().getStringExtra("message_key");
+
+    listaDados.add(newTexto);
+    adaptador.notifyDataSetChanged();
+
+    lista.setAdapter(adaptador);
+
 
         Button vButton = findViewById(R.id.voltar);
 
         vButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
                 Intent intent = new Intent(getApplicationContext(), LayoutHome.class);
                 startActivity(intent);
             }
